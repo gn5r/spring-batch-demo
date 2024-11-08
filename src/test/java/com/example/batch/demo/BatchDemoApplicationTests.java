48ba8f5aa637dbffcd512f8b30ac520c3b247b49
gn5r <shangyuan.tuolang@gmail.com>
2024-11-08 20:15:45 +0900

    create project

---
+++ src/test/java/com/example/batch/demo/BatchDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.batch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchDemoApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
